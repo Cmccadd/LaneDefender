ad11dba2eef3ec8c30be51857a10779202845d0e
Cmccadd <cmccadd@mail.bradley.edu>
2024-09-02 01:43:26 -0500

    Better Enemies added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,15 +1,81 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float moveSpeed = 2f; // Default speed, can be adjusted per enemy type
+    public float moveSpeed = 2f;
+    public float health = 3f; 
+    public float hitPauseDuration = 1f;
+    public Animator animator;
+
+    private bool isHit = false; 
+    private static readonly int HitHash = Animator.StringToHash("IsHit"); 
 
     void Update()
     {
-        // Move the enemy forward
-        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if (!isHit)
+        {
+            // Move the enemy forward if it is not currently hit
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the collision is with an object tagged as "Bullet"
+        if (collision.gameObject.CompareTag("Bullet") && !isHit)
+        {
+            TakeDamage(1f);
+        }
+    }
+
+    private void TakeDamage(float damage)
+    {
+       
+        health -= damage;
+
+      
+        StartCoroutine(HandleHit());
+
+       
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private IEnumerator HandleHit()
+    {
+        if (!isHit)
+        {
+          
+            SetHit(true);
+
+          
+            yield return new WaitForSeconds(hitPauseDuration);
+
+           
+            SetHit(false);
+        }
+    }
+
+    private void SetHit(bool value)
+    {
+        isHit = value;
+        if (animator != null)
+        {
+            animator.SetBool(HitHash, value); 
+        }
+    }
+
+    private void Die()
+    {
+      
+        if (animator != null)
+        {
+            animator.SetTrigger("Die");
+        }
+
+        Destroy(gameObject, 1f); 
     }
 }
-
